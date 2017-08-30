e9606e1ab1c1983c86a86376cbe05a3430378884
jnickell <nickell75@yahoo.com>
2017-08-30 15:58:30 -0700

    Updates user contoller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,22 @@
 class UserController < ApplicationController
+
+	def new
+		@user = User.new		
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.user_id
+			redirect_to home_url
+		else
+			@errors = @user.errors.full_messages
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 	'movies/index'
+  root 	'movies#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#delete'
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  
 end
