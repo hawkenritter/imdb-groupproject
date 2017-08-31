2d66ed5153e6086be2987ccdcf10e4ae77606633
jnickell <nickell75@yahoo.com>
2017-08-31 11:25:51 -0700

    starts work on ajax for comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to profile_path
+			redirect_to :profile
 		else
 			@errors = @user.errors.full_messages
-			render signup_path
+			render :new
 		end
 	end
 
