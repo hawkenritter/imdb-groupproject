955b9357ae2e44da880b329657ecc8f76e752b47
jnickell <nickell75@yahoo.com>
2017-08-30 17:20:09 -0700

    Fix user/session errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.user_id
-			redirect_to home_url
+			session[:user_id] = @user.id
+			redirect_to profile_path
 		else
 			@errors = @user.errors.full_messages
-			render 'new'
+			render signup_path
 		end
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-	validate :username, :email, presence: true
-	validate :username, :email, uniqueness: true
+	validates :username, :email, presence: true
+	validates :username, :email, uniqueness: true
 
-	has_secured_password
+	has_secure_password
 end
